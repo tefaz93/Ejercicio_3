677c50261fc684c79fa569b3ef52ba296220941a
Stephanie Gonzalez Santos <stephanie19_93@hotmail.com>
2015-10-29 15:05:14 -0600

    agregue script de juego der izq

---
+++ Personaje/Assets/scripts/botones.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class botones : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//moveForward ();
+	}
+	//funcion que mueve al personaje
+	public void moveLe ()
+	{
+		//Debug.Log ("Hola");
+		//Mover posicion de forma constante
+		this.transform.Translate( (Time.deltaTime * speed) * Vector3.left);
+	}
+	//funcion que mueve al personaje
+	public void moveR ()
+	{
+		//Vector3.forward
+		//Vector3.up
+		//vector3.down
+		this.transform.Translate( (Time.deltaTime * speed) * Vector3.right);
+	}
+}
--- Personaje/Assets/scripts/move.cs
+++ Personaje/Assets/scripts/move.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class move : MonoBehaviour {
-
+	public float speed;
 	// Use this for initialization
 	void Start () {
-
+		speed = 5;
 	}
 	
 	// Update is called once per frame
@@ -18,6 +18,11 @@ public class move : MonoBehaviour {
 		Debug.Log ("Hola");
 		//Mover posicion de forma constante
 		//this.transform.position = new Vector3 (5,0,1);
-		this.transform.position = Time.deltaTime * Vector3.right;
+		//this.transform.position = Time.deltaTime * Vector3 right;
+
+		//Vector3.forward
+		//Vector3.up
+		//vector3.down
+		this.transform.Translate( (Time.deltaTime * speed) * Vector3.forward);
 	}
 }
