950fcfe09ddb4f441ed4ebda74bd24d09dde8e05
Stephanie Gonzalez Santos <stephanie19_93@hotmail.com>
2015-10-29 14:27:13 -0600

    modifique script

---
+++ Personaje/Assets/scripts/move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveForward ();
+	}
+	//funcion que mueve al personaje
+	public void moveForward ()
+	{
+		Debug.Log ("Hola");
+		//Mover posicion de forma constante
+		//this.transform.position = new Vector3 (5,0,1);
+		this.transform.position = Time.deltaTime * Vector3.right;
+	}
+}
